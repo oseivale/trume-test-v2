a09550258e1408ad5dc7b10d45273b51d045c217
Val O <valerie.osei@gmail.com>
2024-09-16 19:01:19 -0400

    updating container styling

--- src/components/LogoCustomizer.js
+++ src/components/LogoCustomizer.js
@@ -1,40 +1,3 @@
-// import { useRef } from 'react';
-// import html2canvas from 'html2canvas';
-
-// export default function LogoDesigner({ onImageUpload }) {
-//   const logoRef = useRef(null);
-
-//   const generateImage = async () => {
-//     const canvas = await html2canvas(logoRef.current);
-//     const imageUrl = canvas.toDataURL('image/png');
-//     const uploadedUrl = await uploadImageToCloudinary(imageUrl);
-//     onImageUpload(uploadedUrl);
-//   };
-
-//   const uploadImageToCloudinary = async (imageData) => {
-//     const formData = new FormData();
-//     formData.append('file', imageData);
-//     formData.append('upload_preset', 'trume-test'); // Replace with Cloudinary preset
-
-//     const response = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`, {
-//       method: 'POST',
-//       body: formData,
-//     });
-
-//     const data = await response.json();
-//     return data.secure_url; // Return the Cloudinary URL
-//   };
-
-//   return (
-//     <div>
-//       <div ref={logoRef} style={{ width: '300px', height: '150px', backgroundColor: '#333' }}>
-//         <h1 style={{ color: '#fff' }}>Custom Logo</h1>
-//       </div>
-//       <button onClick={generateImage}>Generate and Upload Logo</button>
-//     </div>
-//   );
-// }
-
 import { useEffect, useRef, useState } from "react";
 import html2canvas from "html2canvas";
 import { uploadImageToCloudinary } from "../lib/cloudinary";
@@ -42,6 +5,8 @@ import { data } from "../data";
 import styles from "./styles.module.css";
 import { toPng, toJpeg } from "html-to-image";
 import { roboto_condensed } from "@/fonts";
+import { useRouter } from 'next/router';
+
 
 const ClearIcon = () => (
   <svg
@@ -59,6 +24,12 @@ const ClearIcon = () => (
 );
 
 export default function LogoCustomizer() {
+  const router = useRouter();
+  const { productId, variantId, productTitle, image } = router.query;
+
+  console.log('productId', productId)
+  console.log('variantId', variantId)
+
   // Create a reference to the logo div
   const logoRef = useRef(null);
   const [imageData, setImageData] = useState("");
